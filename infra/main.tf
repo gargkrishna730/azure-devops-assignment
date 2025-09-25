3950342e49853a3271ac407e1e7265ebbbc1f7ba
Krishna Garg <krishna@tyke.ai>
2025-09-25 23:13:28 +0530

    fix(depreceted-name): fix terraform module name

--- infra/main.tf
+++ infra/main.tf
@@ -21,19 +21,15 @@ resource "azurerm_service_plan" "asp" {
   name                = "asp-hello-world-app"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  kind                = "Linux"
-  reserved            = true
-  sku {
-    tier = "Basic"
-    size = "B1"
-  }
+  os_type             = "Linux"
+  sku_name            = "B1"
 }
 
 resource "azurerm_app_service" "app" {
   name                = "app-hello-world-unique"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.asp.id
+  app_service_plan_id = azurerm_service_plan.asp.id
 
   site_config {
     linux_fx_version = "NODE|18-lts"
