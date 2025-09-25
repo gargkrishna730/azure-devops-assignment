fa3949230d0106f57dedef1e732fbaf01039267d
Krishna Garg <krishna@tyke.ai>
2025-09-25 23:20:52 +0530

    fix(depreceted-name): fix resource quota

--- infra/main.tf
+++ infra/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_service_plan" "asp" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
-  sku_name            = "B1"
+  sku_name            = "F1"
 }
 
 resource "azurerm_app_service" "app" {
