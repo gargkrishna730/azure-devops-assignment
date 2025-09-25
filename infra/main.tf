2f1f61ff5c2634a70d3a86d265b0467f3c97a65d
Krishna Garg <krishna@tyke.ai>
2025-09-26 00:02:15 +0530

    fix(terraform script): fixing terraform and workflow file

--- infra/main.tf
+++ infra/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
   required_version = ">= 1.3.0"
 }
@@ -12,23 +16,34 @@ provider "azurerm" {
   features {}
 }
 
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
 # Resource Group
 resource "azurerm_resource_group" "main" {
   name     = "rg-hello-world-app"
-  location = "West US 2"
+  location = "East US"
 }
 
-# Static Web App (using correct resource)
-resource "azurerm_static_web_app" "main" {
-  name                = "hello-world-app"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  sku_tier            = "Free"
+# Storage Account for Static Website
+resource "azurerm_storage_account" "main" {
+  name                     = "helloworldapp${random_string.suffix.result}"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
 # Outputs
-output "web_app_url" {
-  value = azurerm_static_web_app.main.default_host_name
+output "website_url" {
+  value = azurerm_storage_account.main.primary_web_endpoint
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.main.name
 }
 
 output "resource_group" {
