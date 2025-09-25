9d1b8a45264255c4873c7987c88ed2888e288872
Krishna Garg <krishna@tyke.ai>
2025-09-25 23:51:04 +0530

    fix(terraform script): fixing terraform and workflow file

--- infra/main.tf
+++ infra/main.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
   }
   required_version = ">= 1.3.0"
 }
@@ -16,60 +12,25 @@ provider "azurerm" {
   features {}
 }
 
-# Random suffix for unique names
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-  upper   = false
-}
-
 # Resource Group
 resource "azurerm_resource_group" "main" {
   name     = "rg-hello-world-app"
-  location = "East US 2"
-
-  tags = {
-    Environment = "dev"
-    Project     = "hello-world"
-    ManagedBy   = "terraform"
-  }
+  location = "East US"
 }
 
-# Static Web App
-resource "azurerm_static_site" "main" {
-  name                = "swa-hello-world-${random_string.suffix.result}"
+# Static Web App (using correct resource)
+resource "azurerm_static_web_app" "main" {
+  name                = "hello-world-app"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
-  
-  # Free tier
-  sku_tier = "Free"
-  sku_size = "Free"
-
-  tags = {
-    Environment = "dev"
-    Project     = "hello-world"
-    ManagedBy   = "terraform"
-  }
+  sku_tier            = "Free"
 }
 
 # Outputs
-output "resource_group_name" {
-  description = "Name of the resource group"
-  value       = azurerm_resource_group.main.name
+output "web_app_url" {
+  value = azurerm_static_web_app.main.default_host_name
 }
 
-output "static_web_app_name" {
-  description = "Name of the Static Web App"
-  value       = azurerm_static_site.main.name
-}
-
-output "static_web_app_url" {
-  description = "Default URL of the Static Web App"
-  value       = "https://${azurerm_static_site.main.default_host_name}"
-}
-
-output "deployment_token" {
-  description = "Deployment token for the Static Web App"
-  value       = azurerm_static_site.main.api_key
-  sensitive   = true
+output "resource_group" {
+  value = azurerm_resource_group.main.name
 }
